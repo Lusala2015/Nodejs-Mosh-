9d4bbe0cfd9183bd43eee433e5ea53df5d50df22
lusalamark17@gmail.com <lusalamark17@gmail.com>
2020-05-15 14:32:32 +0300

    Mastering Node

---
+++ 2.0 GlobalObject/app.js
@@ -0,0 +1,9 @@
+console.log();//global object
+
+setTimeout();//call a hunction after a delay
+clearTimeout();//
+
+setInterval();// repeatedly call a function after a given delay
+clearInterval();// to stop a function from being callled repeatedly
+
+window.console.log();
---
+++ 4.0 Creating a module/logger.js
@@ -0,0 +1,6 @@
+var url = 'http://mylogger.io/log';
+function log(message){
+    //send a HTTP request
+    console.log(message);
+}
+module.export.log=log;
