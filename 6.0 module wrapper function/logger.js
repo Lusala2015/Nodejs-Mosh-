42b46f212f179609d93280ea193dfc5f7c5f3de8
Lusala2015 <lusalamark17@gmail.com>
2020-06-20 10:10:07 +0300

    creating a syntactical error

--- 6.0 module wrapper function/logger.js
+++ 6.0 module wrapper function/logger.js
@@ -1,8 +1,9 @@
-var x=;
- var url = 'http://mylogger.io/log';
+var x =;
+var url = 'http://mylogger.io/log';
  
 function log(message){
     //send a HTTP request
     console.log(message);
 }
-module.export.log = log;
+
+module.exports= log;
