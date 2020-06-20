2a66eb0d6a6a199f82238ca39d7b63cfac3cf425
Lusala2015 <lusalamark17@gmail.com>
2020-06-20 09:17:17 +0300

    commenting

--- 4.0 Creating a module/logger.js
+++ 4.0 Creating a module/logger.js
@@ -3,4 +3,7 @@ function log(message){
     //send a HTTP request
     console.log(message);
 }
-module.export.log=log;
+
+module.exports.log = log;// implementation for public interface 
+//module.exports.url = url;//implimentation details that we need not to know about?!!
+//module.exports.endPoint = url;// you can change the name of the item being exported.
