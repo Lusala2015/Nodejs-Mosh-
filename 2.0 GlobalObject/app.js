289e20049032ae8fc3d55ba38774e2b9893c6d38
Lusala2015 <lusalamark17@gmail.com>
2020-06-20 09:02:15 +0300

    global object changes

--- 2.0 GlobalObject/app.js
+++ 2.0 GlobalObject/app.js
@@ -7,3 +7,9 @@ setInterval();// repeatedly call a function after a given delay
 clearInterval();// to stop a function from being callled repeatedly
 
 window.console.log();
+
+
+// gives undefined in the console 
+// variables anfd functions defined here are  they are not added to the global object, they are only scoped to this file  app.js
+var message = '';
+console.log(global.message);
