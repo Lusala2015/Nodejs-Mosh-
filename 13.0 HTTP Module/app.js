b67cc4f5c2600a4771b884fbc595dcaf5af225fb
Lusala2015 <lusalamark17@gmail.com>
2020-06-25 13:10:43 +0300

    building a web server with node

--- 13.0 HTTP Module/app.js
+++ 13.0 HTTP Module/app.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
 
-server.on('connection', (socket) => {
-    console.log('New connection ...')
+
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3,4,5,6]));
+        res.end();
+    }
 });
+
 server.listen(3000);
 
 console.log('Listening on port 3000 ...');
