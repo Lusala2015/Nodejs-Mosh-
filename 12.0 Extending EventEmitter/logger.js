4d83e07e75cc446e2a8735d1cd7f9f8beee9c150
Lusala2015 <lusalamark17@gmail.com>
2020-06-25 10:01:06 +0300

    only one event emitter is called

---
+++ 12.0 Extending EventEmitter/logger.js
@@ -0,0 +1,14 @@
+const { EventEmitter } = require("events");
+const emitter = new EventEmitter();
+
+
+var url = 'http://mylogger.io/log';
+
+function log(message){
+    //send a HTTP request
+    console.log(message);
+    
+    emitter.emit('messageLogged',{id: 1, url: 'http://'});
+}
+
+module.exports = log;
