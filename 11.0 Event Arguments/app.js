9b98168359252e2f151f3fe2a9dcf82430902d54
Lusala2015 <lusalamark17@gmail.com>
2020-06-25 08:08:32 +0300

    event arg

---
+++ 11.0 Event Arguments/app.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', function(arg){
+    console.log('Listener called', arg);
+}) ;
+
+
+emitter.emit('messageLogged', { id: 1, url:'http://'});
