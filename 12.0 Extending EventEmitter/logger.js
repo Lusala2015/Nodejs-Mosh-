20cae6a3720be7b174462d1361636863a0a247ba
Lusala2015 <lusalamark17@gmail.com>
2020-06-25 10:08:00 +0300

    to enable the logger class to have all the capabilities of the EventEmitter is by using  "extends"

--- 12.0 Extending EventEmitter/logger.js
+++ 12.0 Extending EventEmitter/logger.js
@@ -8,7 +8,6 @@ class Logger extends EventEmitter {
     log(message){
         //send a HTTP request
         console.log(message);
-        
         emitter.emit('messageLogged',{id: 1, url: 'http://'});
     }
 }
