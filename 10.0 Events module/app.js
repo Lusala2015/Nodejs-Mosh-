fa54b8f4fedf56ba28ae244f6ead0cd1cc5ace2f
Lusala2015 <lusalamark17@gmail.com>
2020-06-25 07:21:35 +0300

    basics of calling and handling the event emitter class

--- 10.0 Events module/app.js
+++ 10.0 Events module/app.js
@@ -1,5 +1,10 @@
 const EventEmitter = require('events');
 
-const emitter = new EventEmiter();
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', function(){
+    console.log('Listener called');
+}) ;
+
 
 emitter.emit('messageLogged');
